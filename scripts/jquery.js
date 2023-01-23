eea4003f5ef22637c8a8cf9a035b80264e8771eb
0opsii <flores.priscila91@gmail.com>
2023-01-23 20:43:28 -0300

    dev

--- scripts/jquery.js
+++ scripts/jquery.js
@@ -1,5 +1,7 @@
-$(document).ready(function(){
-    $("#enviar").click(function(){
+$(document).ready(function () {
+    $("#enviar").click(function () {
         alert("Gracias por contactarte con InclusiveTrip, revisaremos tu mensaje y responderemos a la brevedad.");
     });
 });
+
+
