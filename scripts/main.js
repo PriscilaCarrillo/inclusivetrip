ab64fcb31a12ba4244e8ed2d9f408aee17ada392
Priscila <flores.priscila91@gmail.com>
2023-04-25 13:48:15 -0400

    avances visual proyecto final

--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,5 @@
 let contenedor = document.getElementById('contenedor')
-const semana = ["Domingo", "Lunes", "Martes", "Miércoles", "Jueves", "Viernes", "Sábado"];
+const semana = ["Domingo","Lunes","Martes","Miércoles","Jueves","Viernes","Sábado"];
 
 const dia = new Date();
 let dias = semana[dia.getDay()]
@@ -7,7 +7,7 @@ let dias = semana[dia.getDay()]
 const diaNum = new Date()
 let diasNum = diaNum.getDate()
 
-const meses = ["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"]
+const meses = ["Enero","Febrero","Marzo","Abril","Mayo","Junio","Julio","Agosto","Septiembre","Octubre","Noviembre","Diciembre"]
 
 const month = new Date()
 let months = meses[month.getMonth()]
@@ -18,4 +18,4 @@ let horaAhora = hora.toLocaleTimeString('es-CL')
 
 const annio = new Date();
 let annios = annio.getFullYear()
-contenedor.innerHTML = '' + dias + '  ' + diasNum + " de " + months + "-" + horaAhora
+contenedor.innerHTML = '' + dias + '  ' +diasNum + " de " + months + "" +  annios  +  '-'  + horaAhora
